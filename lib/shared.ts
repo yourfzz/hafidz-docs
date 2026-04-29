a12dc2259601287f8a33d03af3755070ef8ff6cb
Hafidz25 <hafidzalif80@gmail.com>
2026-04-29 15:28:57 +0700

    Update GitHub configuration with correct repository and branch names

--- lib/shared.ts
+++ lib/shared.ts
@@ -6,6 +6,6 @@ export const docsContentRoute = "/llms.mdx/docs";
 // fill this with your actual GitHub info, for example:
 export const gitConfig = {
   user: "yourfzz",
-  repo: "fumadocs",
-  branch: "main",
+  repo: "hafidz-docs",
+  branch: "master",
 };
